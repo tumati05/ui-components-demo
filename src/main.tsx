9fbced30dca38c43917d5506d920fe081bd45880
Tumati Tejasri <tumatitejasri3@gmail.com>
2025-08-18 22:39:30 +0530

    Resolve merge conflicts: delete index.tsx, fix main.tsx and tsconfig.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+<<<<<<< Updated upstream
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+=======
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+>>>>>>> Stashed changes
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
